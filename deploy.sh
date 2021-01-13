bf041753642cda5ae6c9f0af4d300ceb32ce3357
CurryAri <arieleshet@gmail.com>
2021-01-13 15:34:06 +0200

    moved aws installation to before_deploy section

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,11 @@
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-sudo mkdir .aws
-sudo chown -R travis:travis /home/travis
-aws configure set aws_access_key_id \$AWS_ACCESS_KEY_ID
-aws configure set default.region us-east-2
-aws configure set aws_secret_access_key \$AWS_SECRET_ACCESS_KEY
-#echo "[default]" > .aws/config
-#echo "aws_access_key_id = \$AWS_ACCESS_KEY_ID" >> .aws/config
-#echo "aws_secret_access_key = \$AWS_SECRET_ACCESS_KEY" >> .aws/config
-#echo "region = us-east-2" >> .aws/config
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sudo ./aws/install
+#sudo mkdir .aws
+#sudo chown -R travis:travis /home/travis
+#aws configure set aws_access_key_id \$AWS_ACCESS_KEY_ID
+#aws configure set default.region us-east-2
+#aws configure set aws_secret_access_key \$AWS_SECRET_ACCESS_KEY
 cmdid=$(aws ssm send-command --instance-ids "i-02b253149b93a7776" --document-name "AWS-RunShellScript" --parameters commands="sudo kubectl set image deployments/nginx-deployment nginx=curryari/tompencil:$SHA" --query "Command.CommandId" --output text)
 aws ssm list-command-invocations --command-id "$cmdid" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text
 #kubectl apply -f k8s

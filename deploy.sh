90a37a9ef86e6473942f557829bec80426daa04f
CurryAri <arieleshet@gmail.com>
2021-01-13 15:13:45 +0200

    deleted extra spaces in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2    .zip"
-ls
-pwd
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 sudo mkdir .aws

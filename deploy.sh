9f8d661a2f7af16a29d4e8f2d8d1ead25353996d
CurryAri <arieleshet@gmail.com>
2021-01-17 10:58:57 +0200

    updated index.html

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 echo "deploy script is running"
 cmdid=$(aws ssm send-command --instance-ids "i-02b253149b93a7776" --document-name "AWS-RunShellScript" --parameters commands="sudo kubectl set image deployments/nginx-deployment nginx=curryari/tompencil:$SHA" --query "Command.CommandId" --output text)
 aws ssm list-command-invocations --command-id "$cmdid" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text
+
+
+
+
 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 #unzip awscliv2.zip
 #sudo ./aws/install
@@ -11,5 +15,3 @@ aws ssm list-command-invocations --command-id "$cmdid" --details --query "Comman
 #aws configure set aws_secret_access_key \$AWS_SECRET_ACCESS_KEY
 #kubectl apply -f k8s
 #kubectl set image deployments/server-deployment server=curryari/multi-server:$SHA
-#kubectl set image deployments/client-deployment client=curryari/multi-client:$SHA
-#kubectl set image deployments/worker-deployment worker=curryari/multi-worker:$SHA
